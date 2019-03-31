364975666135f0c3ee18e024b4b25a98996b205c
qfstudy <qifeistudy@gmail.com>
2019-03-31 19:23:44 +0800

    完成写文章页面布局

--- routers/writeArticle.js
+++ routers/writeArticle.js
@@ -1,7 +1,6 @@
 const router = require('koa-router')()
 const mysqlModel = require('../lib/mysql.js')
 const moment = require('moment')
-// const hadLogin = require('../middlewares/check.js').hadLogin
 const noLogin = require('../middlewares/check.js').noLogin
 // const md = require('markdown-it')()
 const  showdown  = require('showdown') 
@@ -23,29 +22,12 @@ router.post('/write', async(ctx, next) => {
   let time = moment().format('YYYY-MM-DD HH:mm:ss')
   let avatar
  
-  newContent = content.replace(/[<">']/g, (target) => { 
-    return {
-      '<': '&lt;',
-      '"': '&quot;',
-      '>': '&gt;',
-      "'": '&#39;'
-    }[target]
-  }),
-  newTitle = title.replace(/[<">']/g, (target) => {
-    return {
-      '<': '&lt;',
-      '"': '&quot;',
-      '>': '&gt;',
-      "'": '&#39;'
-    }[target]
-  });
-  //console.log([name, newTitle, content, id, time])
   await mysqlModel.searchUser(ctx.session.user)
     .then(res => {
       // console.log(res[0]['avatar'])
       avatar = res[0]['avatar']       
     })
-  await mysqlModel.addArticle([name, newTitle, converter.makeHtml(content), content, id, time,avatar])
+  await mysqlModel.addArticle([name, title, converter.makeHtml(content), content, id, time,avatar])
     .then(() => {
       ctx.body = {
         code: 200,
